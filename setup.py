0dfdf8a2bf29448b5a22bd32897e3260cbddde58
Stefan Nožinić <stefan@lugons.org>
2015-04-27 00:23:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyfb',
+  packages = ['pyfb'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A facebook library with classes for various facebook stuff',
+  author = 'Stefan Nozinic',
+  author_email = 'stefan@lugons.org',
+  url = 'https://github.com/fantastic001/pyfb', # use the URL to the github repo
+  download_url = 'https://github.com/fantastic001/pyfb/tarball/0.1',
+  keywords = ['facebook', 'chat'], # arbitrary keywords
+  classifiers = [],
+)
